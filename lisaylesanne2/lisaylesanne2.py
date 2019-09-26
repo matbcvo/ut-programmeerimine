82f837b35621fecd0cc096f53d262e593481e0bc
Martin Vooremäe <martin.vooremae@gmail.com>
2019-09-26 22:45:27 +0300

    2. lisaülesanne - Teksti korrastamine

---
+++ lisaylesanne2/lisaylesanne2.py
@@ -0,0 +1,26 @@
+import textwrap
+
+lähtefail = input("Palun sisesta lähtefaili nimi: ")
+sihtfail = input("Palun sisesta sihtfaili nimi: ")
+sf_realaius = int(input("Palun sisesta sihtfaili realaius: "))
+
+with open(lähtefail, encoding="utf-8") as lf:
+    sisu = lf.read().rstrip()
+
+print("Esialgses tekstis oli " + str(len(sisu)) + " tähemärki.")
+
+sisu = ' '.join(sisu.split())
+
+print("Lõplikus tekstis on " + str(len(sisu)) + " tähemärki.")
+
+print("Tekstis on " + str(len(sisu.split())) + " sõna.")
+
+msisu = textwrap.wrap(sisu, sf_realaius)
+
+with open(sihtfail, "a") as sf:
+    for x in range(len(msisu)):
+        sf.write(msisu[x])
+        if x is not len(msisu)-1:
+            sf.write("\n")
+
+print("Sihtfaili salvestati " +  str(len(msisu)) + " rida laiusega " + str(sf_realaius) + " tähemärki.")
